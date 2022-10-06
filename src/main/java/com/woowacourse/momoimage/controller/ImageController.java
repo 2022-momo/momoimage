dafaa694f67b996527f21ea1818a3da6f95800d3
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-10-06 18:33:52 +0900

    refactor: 불필요 import문 제거

--- src/main/java/com/woowacourse/momoimage/controller/ImageController.java
+++ src/main/java/com/woowacourse/momoimage/controller/ImageController.java
@@ -2,15 +2,10 @@ package com.woowacourse.momoimage.controller;
 
 import java.net.URI;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
